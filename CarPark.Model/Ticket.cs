d039d08f4a8aadf74889c62464e4eba92b2bd13f
Prasert Krinuwat <Prasert.k@connex.local>
2016-04-04 14:14:08 +0700

    checkin all

---
+++ CarPark.Model/Ticket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPark.Model
+{
+    public class Ticket
+    {
+        public DateTime DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+        public decimal ParkingFee {
+
+            get
+            {
+                TimeSpan ts = DateOut - DateIn;
+                ts = ts.Add(TimeSpan.FromMinutes(-15));
+                if (ts.TotalMinutes <= 0)
+                    return 0m;
+                else if (ts.TotalHours <= 3.0)
+                    return 50m;
+                else                
+                    return 50m + ((decimal)Math.Ceiling(ts.TotalHours - 3 ) * 30m);
+                   
+             }
+        }
+        public string PlateNo { get; set; }
+    }
+
+
+   
+}
